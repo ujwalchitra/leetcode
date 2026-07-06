65ff05172bbc7bf44d755c4abde89fc904e86167
ujwalchitra <ujwal.chitra.pg25@nsut.ac.in>
2026-07-07 00:17:30 +0530

    Time: 7 ms (75.53%), Space: 64.8 MB (77.77%) - LeetHub

---
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
+        int ans1=0;
+        int ans2=0;
+        for(int i=0;i<nums1.size();i++){
+            for(int j=0;j<nums2.size();j++){
+                if(nums1[i]==nums2[j]){
+                    ans1++;
+                    break;
+                }
+            }
+        }
+        for(int i=0;i<nums2.size();i++){
+            for(int j=0;j<nums1.size();j++){
+                if(nums2[i]==nums1[j]){
+                    ans2++;
+                    break;
+                }
+            }
+        }
+        vector<int>v;
+        v.push_back(ans1);
+        v.push_back(ans2);
+        return v;
+    }
+};

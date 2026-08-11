366e3a3d1afa9fc4b6834bf54fdc8db2a3d7c951
Ujwal Chitra <ujwal.chitra.pg25@nsut.ac.in>
2026-08-11 22:53:15 +0530

    Time: 18 ms (22.66%), Space: 8.2 MB (11.3%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int dpp(vector<int>&dp,int n){
+        if(n<=1){
+            return n;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dpp(dp,n-1)+dpp(dp,n-2);
+    }
+    int fib(int n) {
+        vector<int>dp(n+1,-1);
+        return dpp(dp,n);
+    }
+};

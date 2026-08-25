63bae547e122928433e91ee4289942cd4b7ee2ae
Ujwal Chitra <ujwal.chitra.pg25@nsut.ac.in>
2026-08-26 04:06:48 +0530

    Time: 3 ms (18.86%), Space: 71.7 MB (81.41%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int a=0;
         int sum=INT_MIN;
+        int k=0;
         for(int i=0;i<nums.size();i++){
-            a=a+nums[i];
-            sum=max(sum,a);
-            if(a<0){
-                a=0;
+                k=k+nums[i];
+                sum=max(sum,k);
+                if(k<0){
+                   k=0;
+                }
             }
-        }
+        
+        
         return sum;
     }
 };

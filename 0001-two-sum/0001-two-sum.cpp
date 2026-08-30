ce837cb63c479f7545fed645b9a94c9de3f19e0a
Ujwal Chitra <ujwal.chitra.pg25@nsut.ac.in>
2026-08-30 23:09:51 +0530

    Time: 78 ms (8.78%), Space: 14.1 MB (74.83%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int>m;
 
-        for(int i=0;i<nums.size();i++){
-            int c=target-nums[i];
-            if(m.count(c)){
-                return {m[c],i};
-        
+        vector<int>v;
+         for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
+                    v.push_back(i);
+                    v.push_back(j);
+                    break;
+                }
             }
-            m[nums[i]]=i;
-        }
-        return {};
+         }
+        return v;
     }
 };

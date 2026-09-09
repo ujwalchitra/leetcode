a86467ce4b42c2393595f929b13e6cd644ea8fdc
Ujwal Chitra <ujwal.chitra.pg25@nsut.ac.in>
2026-09-09 22:36:51 +0530

    Time: 11 ms (15.1%), Space: 39.7 MB (8.4%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -6,34 +6,28 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    void find(TreeNode* root, unordered_map<int, int>& v) {
-        if (root == NULL) {
-            return;
-        }
-        v[root->val]++;
-        find(root->left, v);
-        find(root->right, v);
-    }
     bool findTarget(TreeNode* root, int k) {
-        unordered_map<int, int> v;
-        find(root, v);
-        if(v.size()<=1){
-            return false;
-        }
-        for (auto const& pair : v) {
-            int a = k - pair.first;
-            if (v.count(a)) {
-                if(a!=pair.first||v[a]>1){
+        unordered_map<int,int>m;
+        solve(m,root);
+        for(const auto &pair:m){
+            int a=k-pair.first;
+            if(a!=pair.first &&m.find(a)!=m.end()){
                 return true;
-                break;}
             }
         }
         return false;
+    }
+    void solve(unordered_map<int,int>&m,TreeNode* root){
+        if(root==NULL){
+            return;
+        }
+        m[root->val]++;
+        solve(m,root->left);
+        solve(m,root->right);
     }
 };

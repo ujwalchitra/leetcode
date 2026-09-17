ca7acd3829c11ea9183e330a9019b36441b2a830
Ujwal Chitra <ujwal.chitra.pg25@nsut.ac.in>
2026-09-17 23:17:36 +0530

    Time: 0 ms (100%), Space: 97.4 MB (26.35%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-       int start=prices[0];
-       int sum=0;
-       for(int i=1;i<prices.size();i++){
-        if(start>prices[i]){
-            start=prices[i];
+        int sum=0;
+        int ans=0;
+        int a=prices[0];
+        for(int i=1;i<prices.size();i++){
+            if(a<prices[i]){
+                sum=prices[i]-a;
+                ans=max(ans,sum);
+            }
+            else if (a>prices[i]){
+                a=prices[i];
+            }
         }
-        else if(start<prices[i]){
-            sum=max(sum,prices[i]-start);
-        }
-       }
-       return sum;
+        return ans;
     }
 };

7ea98d2ba911482af4c57f6160874545124ff410
Ujwal Chitra <ujwal.chitra.pg25@nsut.ac.in>
2026-09-18 02:45:24 +0530

    Time: 8 ms (6.67%), Space: 9.8 MB (48.34%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,21 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        map<char, int> m;
+        if (s.size() != t.size()) {
+            return false;
+        }
+        for (int i = 0; i < s.size(); i++) {
+            m[s[i]]++;
+            m[t[i]]--;
+        }
+   
+        for (const auto& pair : m) {
+            if (pair.second!=0) {
+                return false;
+                break;
+            }
+        }
+        return true;
     }
 };

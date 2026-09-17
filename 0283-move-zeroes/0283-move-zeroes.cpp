e2cb32d85eafad1e9c9c540a860dc1df6397fb49
Ujwal Chitra <ujwal.chitra.pg25@nsut.ac.in>
2026-09-17 19:15:49 +0530

    Time: 0 ms (100%), Space: 23.8 MB (82.73%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        if(nums.size()==1){
-            return;
-        }
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==0){
-                int j=i+1;
-                while(nums[j]==0){
-                    if(j+1<nums.size()){
-                    j++;}
-                    else{
-                        break;
-                    }
-                }
+        int j=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
                 swap(nums[i],nums[j]);
+                j++;
             }
         }
-        
     }
 };

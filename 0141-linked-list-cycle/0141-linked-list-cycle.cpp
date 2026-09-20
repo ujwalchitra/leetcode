71c3dcaad8743bf166351fe0ec7c82efa33b4abd
Ujwal Chitra <ujwal.chitra.pg25@nsut.ac.in>
2026-09-20 16:27:28 +0530

    Time: 12 ms (40.6%), Space: 11.8 MB (52.97%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,20 +9,20 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode* curr=head;
-        ListNode* durr=head;
-        if(head==NULL||head->next==NULL){
-            return false;
-        }
+        ListNode *curr=head;
 
-        while(durr!=NULL&&durr->next!=NULL){
+         ListNode *durr=head;
+         if(head==NULL||head->next==NULL){
+            return false;
+         }
+         while(durr->next!=NULL&&durr->next->next!=NULL){
             curr=curr->next;
             durr=durr->next->next;
             if(curr==durr){
                 return true;
                 break;
             }
-        }
-        return false;
+         }
+         return false;
     }
 };

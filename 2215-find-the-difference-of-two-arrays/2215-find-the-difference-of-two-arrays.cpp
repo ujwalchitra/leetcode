85523eaaefddf7204a3d49851f9e1800e5dbba0c
Ujwal Chitra <ujwal.chitra.pg25@nsut.ac.in>
2026-09-21 19:45:29 +0530

    Time: 35 ms (16.19%), Space: 40.9 MB (6.52%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -1,41 +1,50 @@
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> m;
-        unordered_map<int, int> n;
-        unordered_map<int, int> k;
-        unordered_map<int, int> l;
+        unordered_set<int> m;
+        unordered_set<int> n;
+        unordered_set<int> k;
+        unordered_set<int> l;
+
         for (int i = 0; i < nums1.size(); i++) {
-            m[nums1[i]]++;
+            m.insert(nums1[i]);
         }
+
         for (int i = 0; i < nums2.size(); i++) {
-            n[nums2[i]]++;
+            n.insert(nums2[i]);
         }
+
         vector<int> c;
         vector<int> v;
         vector<vector<int>> ans;
+
         for (int i = 0; i < nums2.size(); i++) {
             if (m.find(nums2[i]) != m.end()) {
                 continue;
             } else {
-                k[nums2[i]]++;
+                k.insert(nums2[i]);
             }
         }
+
         for (int i = 0; i < nums1.size(); i++) {
             if (n.find(nums1[i]) != n.end()) {
                 continue;
             } else {
-                l[nums1[i]]++;
+                l.insert(nums1[i]);
             }
         }
-        for (const auto& pair : k) {
-            c.push_back(pair.first);
+
+        for (const auto& x : k) {
+            c.push_back(x);
         }
-        for (const auto& pair : l) {
-            v.push_back(pair.first);
+
+        for (const auto& x : l) {
+            v.push_back(x);
         }
+
         ans.push_back(v);
         ans.push_back(c);
+
         return ans;
     }
 };

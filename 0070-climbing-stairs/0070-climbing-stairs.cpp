6ace5fdb46c3f9432d7c10c068a5588685d68cba
Ujwal Chitra <ujwal.chitra.pg25@nsut.ac.in>
2026-09-25 15:21:36 +0530

    Time: 0 ms (100%), Space: 8.5 MB (37.39%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    int dpp(vector<int>& dp, int n) {
+    int climbStairs(int n) { 
+        vector<int>dp(n+1,-1);
+        return solve(n,dp); }
+    int solve(int n,vector<int>&dp) {
         if (n == 0) {
             return 1;
         }
-        if (n == 1) {
-            return 1;
-        }
-        if (dp[n] != -1) {
+        if(dp[n]!=-1){
             return dp[n];
         }
-        int left = dpp(dp, n - 1);
-        int right = dpp(dp, n - 2);
+        int left = solve(n - 1,dp);
+        int right = 0;
+        if (n - 2 >= 0) {
+            right = solve(n - 2,dp);
+        }
         dp[n]=left+right;
         return dp[n];
-    }
-    int climbStairs(int n) {
-        vector<int> dp(n + 1, -1);
-        return dpp(dp, n);
     }
 };

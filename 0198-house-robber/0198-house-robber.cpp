b175e72cd6a360531166d697ce3383c7cd1c0091
Ujwal Chitra <ujwal.chitra.pg25@nsut.ac.in>
2026-09-26 23:11:17 +0530

    Time: 0 ms (100%), Space: 10.7 MB (45.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,7 +4,6 @@ public:
         int i=0;
         vector<int>dp(nums.size()+1,-1);
         return solve(i,nums,dp);
-
     }
     int solve(int i,vector<int>& nums,vector<int>&dp){
         if(i>=nums.size()){
@@ -13,9 +12,9 @@ public:
         if(dp[i]!=-1){
             return dp[i];
         }
-        int a=nums[i]+solve(i+2,nums,dp);
-        int b=solve(i+1,nums,dp);
-        dp[i]=max(a,b);
+        int left=nums[i]+solve(i+2,nums,dp);
+        int right=solve(i+1,nums,dp);
+        dp[i]=max(right,left);
         return dp[i];
     }
 };
